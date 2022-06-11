34571705d9dc870a272a13d2b00d2f2515458815
unknown <tilakthapliyal@gmail.com>
2022-06-12 00:38:35 +0200

    advanced calculator

---
+++ calculator.py
@@ -0,0 +1,60 @@
+from tkinter import Y
+
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+def calculator():
+
+  num1 = float(input("what's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+
+  should_continue = True
+
+  while should_continue:
+
+    operations_symbol = input("Pick an operation: ")
+    num2 = float(input("what's the next number?: "))
+    calculation_function = operations[operations_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operations_symbol} {num2} = {answer}")
+
+    choice = input(f"type 'y' to continue with {answer}, or type 'n to exit.: ")
+    if choice == Y:
+      num1 = answer
+    else: 
+      should_conitnue = False
+      calculator()
+
+calculator()
